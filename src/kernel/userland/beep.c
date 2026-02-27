9ae0b16b71b73d97a435b83bef6267bb3304970e
boreddevnl <chris@boreddev.nl>
2026-02-27 23:31:13 +0100

    BEEP fix

--- src/kernel/userland/beep.c
+++ src/kernel/userland/beep.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
     (void)argc; (void)argv;
+    sys_system(14, 392, 400, 0, 0);
     printf("BEEP!\n");
-    sys_system(14, 1000, 100, 0, 0); // SYSTEM_CMD_BEEP (freq, ms)
     return 0;
 }
